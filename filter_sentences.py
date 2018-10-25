1f774b9869ae067814252949a280a319ac696d6d
William Edmisten <wcedmisten@gmail.com>
2018-10-25 11:46:38 -0400

    Create filter_sentences.py
    python script to filter documents with relevant keywords.

---
+++ filter_sentences.py
@@ -0,0 +1,12 @@
+import json
+
+data_list = []
+
+with open('cleaned_big.json') as f:
+	for line in f:
+		data = json.loads(line)
+		sentences = data["Sentences_t"]
+		if sentences != "" and ("quake" in sentences.lower() or "zealand" in sentences.lower()):
+			data_list.append(data)
+			print(json.dumps(data))
+	print(len(data_list))
